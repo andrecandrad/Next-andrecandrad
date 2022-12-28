a7c8691d271e01d40bab0696b1507ae31e9b4f64
andrecandrad <andrecandrad@gmail.com>
2022-12-27 23:45:04 -0300

    Experiencias funcionando e alternando corretamente.

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Section = () => {
-  const [activeJob, setActiveJob] = useState("hotmart");
-
   const experiencias = [
     {
-      name: "hotmart",
+      title: "Jovem Aprendiz",
+      company: "Hotmart",
+      period: "Set 2020 - Ago 2021 (11 meses)",
+      desc: "Ajudei o time nos pagamentos dos nossos produtores e afiliados, atendimento de dúvidas relacionadas a área e ajuda com comprovantes bancários.",
     },
     {
-      name: "4mti",
+      title: "Estagiário em Desenvolvimento",
+      company: "4mti",
+      period: "Fev 2022 - Jun 2022 (5 meses)",
+      desc: "Estágio em desenvolvimento full-stack. Trabalhei com tecnologias como: HTML/CSS,Javascript, jQuery, PHP, Python, MySql.",
     },
     {
-      name: "levty",
+      title: "Estagiário em Desenvolvimento",
+      company: "Levty",
+      period: "Jul 2022 - Hoje (5 meses)",
+      desc: "Estágio técnico em desenvolvimento de software. Trabalho na plataforma SYDLE-ONE com JavaScript utilizando aplicando os conceitos de orientação a objetos, api e banco de dados.",
     },
   ];
 
+  const [activeJob, setActiveJob] = useState(
+    experiencias.find((experiencia) => experiencia.company === "Hotmart")
+  );
+
   function handleJob(job) {
-    setActiveJob(job);
+    let trabalho = experiencias.find(
+      (experiencia) => experiencia.company === job
+    );
+    setActiveJob(trabalho);
   }
 
   let isSelectedJob = null;
@@ -34,19 +48,18 @@ const Section = () => {
             {experiencias.map((experiencia) => {
               return (
                 <li
-                  key={experiencia.name}
-                  {...(activeJob === experiencia.name
+                  key={experiencia.company}
+                  {...(activeJob.company === experiencia.company
                     ? (isSelectedJob =
-                        "cursor-pointer py-6 px-8 font-Poppins text-xl text-purple1 font-semibold border-l-[5px] border-b-[5px] border-purple1")
+                        "cursor-pointer py-6 px-8 font-Poppins text-xl text-purple1 ease-in duration-150 font-semibold border-l-[5px] border-b-[5px] border-purple1")
                     : (isSelectedJob =
-                        "cursor-pointer py-6 px-8 font-Poppins text-xl"))}
+                        "cursor-pointer py-6 px-8 font-Poppins text-xl ease-in duration-150"))}
                   className={isSelectedJob}
                   onClick={() => {
-                    handleJob(experiencia.name);
+                    handleJob(experiencia.company);
                   }}
                 >
-                  {experiencia.name.charAt(0).toUpperCase() +
-                    experiencia.name.slice(1)}
+                  {experiencia.company}
                 </li>
               );
             })}
@@ -55,21 +68,19 @@ const Section = () => {
           <div>
             <div className="mb-6 flex flex-col items-baseline justify-between sm:flex-row">
               <h3 className="mb-6 font-Poppins text-2xl font-normal sm:mb-0">
-                Jovem Aprendiz
+                {activeJob.title}
               </h3>
               <h5 className="text-base font-medium text-gray-600">
-                Set 2020 - Ago 2021 (11 meses)
+                {activeJob.period}
               </h5>
             </div>
 
             <h5 className="mb-6 font-Poppins text-xl font-medium text-purple1">
-              Hotmart
+              {activeJob.company}
             </h5>
 
             <p className="text-lg font-normal text-gray-600 sm:text-xl">
-              Ajudei o time nos pagamentos dos nossos produtores e afiliados,
-              atendimento de dúvidas relacionadas a área e ajuda com
-              comprovantes bancários.
+              {activeJob.desc}
             </p>
           </div>
         </div>
