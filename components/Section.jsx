ededba0e77301cade27dadabeae91586f1c9465b
andrecandrad <andrecandrad@gmail.com>
2022-12-28 02:01:35 -0300

    finalizada a seção de experiências

--- components/Section.jsx
+++ components/Section.jsx
@@ -8,18 +8,47 @@ const Section = () => {
       company: "Hotmart",
       period: "Set 2020 - Ago 2021 (11 meses)",
       desc: "Ajudei o time nos pagamentos dos nossos produtores e afiliados, atendimento de dúvidas relacionadas a área e ajuda com comprovantes bancários.",
+      tags: [
+        "BackOffice",
+        "Bancos",
+        "Excel",
+        "Transações",
+        "Zendesk",
+        "Atendimento",
+      ],
     },
     {
       title: "Estagiário em Desenvolvimento",
       company: "4mti",
       period: "Fev 2022 - Jun 2022 (5 meses)",
-      desc: "Estágio em desenvolvimento full-stack. Trabalhei com tecnologias como: HTML/CSS,Javascript, jQuery, PHP, Python, MySql.",
+      desc: "Estágio técnico em desenvolvimento web full-stack. Trabalhei criando novas funcionalidades e dando manutenção nos softwares da empresa, utilizando tecnologias para web, bancos de dados e metodologias ágeis.",
+      tags: [
+        "Html",
+        "Css",
+        "JavaScript",
+        "jQuery",
+        "MySql",
+        "Python",
+        "Flask",
+        "Php",
+        "Git",
+      ],
     },
     {
       title: "Estagiário em Desenvolvimento",
       company: "Levty",
       period: "Jul 2022 - Hoje (5 meses)",
-      desc: "Estágio técnico em desenvolvimento de software. Trabalho na plataforma SYDLE-ONE com JavaScript utilizando aplicando os conceitos de orientação a objetos, api e banco de dados.",
+      desc: "Estágio técnico em desenvolvimento de software. Trabalho na plataforma SYDLE-ONE aplicando os conceitos de orientação a objetos, API, banco de dados, código limpo e metodologias ágeis.",
+      tags: [
+        "Sydle-One",
+        "POO",
+        "Javascript",
+        "Código Limpo",
+        "Scrum",
+        "Kanban",
+        "Api's",
+        "No-Sql",
+      ],
     },
   ];
 
@@ -43,17 +72,17 @@ const Section = () => {
           Experiências
           <span className="text-purple1">.</span>
         </h1>
-        <div className="flex w-full flex-col gap-10 sm:flex-row">
-          <ul className="w-full shrink-0 rounded-sm shadow-xl sm:basis-72">
+        <div className="flex w-full flex-col gap-10 md:flex-row">
+          <ul className="flex max-h-[233px] w-full shrink-0 flex-col justify-between rounded-sm shadow-xl sm:basis-72">
             {experiencias.map((experiencia) => {
               return (
                 <li
                   key={experiencia.company}
                   {...(activeJob.company === experiencia.company
                     ? (isSelectedJob =
-                        "cursor-pointer py-6 px-8 font-Poppins text-xl text-purple1 ease-in duration-150 font-semibold border-l-[5px] border-b-[5px] border-purple1")
+                        "cursor-pointer py-6 px-8 font-Poppins text-xl text-purple1 ease-in duration-150 font-semibold border-l-[5px] border-b-[5px] border-purple1 hover:font-semibold hover:text-purple1")
                     : (isSelectedJob =
-                        "cursor-pointer py-6 px-8 font-Poppins text-xl ease-in duration-150"))}
+                        "cursor-pointer py-6 px-8 font-Poppins text-xl ease-in duration-150 hover:font-semibold hover:text-purple1"))}
                   className={isSelectedJob}
                   onClick={() => {
                     handleJob(experiencia.company);
@@ -66,7 +95,7 @@ const Section = () => {
           </ul>
 
           <div>
-            <div className="mb-6 flex flex-col items-baseline justify-between sm:flex-row">
+            <div className="mb-6 flex flex-col flex-wrap items-baseline justify-between sm:flex-row">
               <h3 className="mb-6 font-Poppins text-2xl font-normal sm:mb-0">
                 {activeJob.title}
               </h3>
@@ -82,6 +111,16 @@ const Section = () => {
             <p className="text-lg font-normal text-gray-600 sm:text-xl">
               {activeJob.desc}
             </p>
+
+            <div className="mt-6 flex w-full flex-wrap gap-2">
+              {activeJob.tags.map((tag) => {
+                return (
+                  <p className="cursor-pointer rounded-full bg-purple1 px-3 font-medium text-white duration-150 ease-in hover:bg-white hover:text-purple1">
+                    {tag}
+                  </p>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
