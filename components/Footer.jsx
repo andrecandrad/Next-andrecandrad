b9c9e9d7f5679bcc6060d13cb6676b547d871a91
andrecandrad <78825073+andrecandrad@users.noreply.github.com>
2023-01-05 11:46:07 -0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <section className="mt-28 w-full border-b-4 border-purple1">
       <footer className="cont flex flex-col items-center justify-between gap-4 font-Poppins text-xs sm:flex-row sm:gap-0 sm:text-base">
-        <h5 className="flex items-center gap-2">
+        <h5 className="flex flex-col sm:flex-row items-center gap-2">
           Projetado e desenvolvido com <FaHeart className="text-red-400" /> por
           André Andrade
         </h5>
