f13c41cfe6a08613b02d92c6bda2c04ebb1757ae
andrecandrad <78825073+andrecandrad@users.noreply.github.com>
2023-01-05 14:21:59 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default function Document() {
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
         rel="stylesheet"
       />
+      <title>André Andrade - Portfólio</title>
       <body>
         <Main />
         <NextScript />
